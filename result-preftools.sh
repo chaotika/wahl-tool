27b61ec9624fbe645b15c6fd27fb8ce49973d3b3
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2018-11-08 15:34:08 +0000

    use local address

--- result-preftools.sh
+++ result-preftools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p tmp
-wget -q -O tmp/candidates.txt http://localhost:8080/preftools/candidates.txt
-wget -q -O tmp/ballots.txt http://localhost:8080/preftools/ballots.txt
+wget -q -O tmp/candidates.txt http://127.5.6.7:8080/preftools/candidates.txt
+wget -q -O tmp/ballots.txt http://127.5.6.7:8080/preftools/ballots.txt
 lua5.1 ./preftools-v0.9/schulze -c tmp/candidates.txt -b tmp/ballots.txt -d last -q 0
